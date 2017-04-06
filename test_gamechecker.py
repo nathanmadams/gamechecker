8f022d786100bf43e43b66801344c7c1883ef638
Nathan Adams <nathanmadams@gmail.com>
2017-04-06 15:28:11 -0400

    More unit tests.

--- test_gamechecker.py
+++ test_gamechecker.py
@@ -7,8 +7,20 @@ class GameStatusTestCase(unittest.TestCase):
     """Tests for `GameStatus`."""
 
     def test_username_from(self):
-        tree = html.parse('resources/valid_status_page.html')
-        self.assertEquals(GameStatus.username_from(tree), 'test_user')
+        tree = html.parse('resources/current_player.html')
+        self.assertEquals(GameStatus.username_from(tree), 'testuser')
+
+    def test_current_players_from(self):
+        tree = html.parse('resources/current_player.html')
+        self.assertTrue('testuser' in GameStatus.current_players_from(tree))
+
+    def test_user_status(self):
+        with open('resources/current_player.html', 'r') as f:
+            content = f.read()
+        (username, status) = GameStatus.user_status(content)
+        self.assertEquals(username, 'testuser')
+        self.assertTrue(status)
+        
 
 if __name__ == '__main__':
     unittest.main()
